26314e378624fab1bdfb234e3a9cb7b90ea29d5b
Roman <raksenov50@gmail.com>
2025-07-02 18:49:51 +0300

    Engine setup: main code setup for work [#17] maven, spring, dependencies added
    BREAKING CHANGE: Project setup

---
+++ src/test/java/com/drip/CompetitionEngine/CompetitionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drip.CompetitionEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
