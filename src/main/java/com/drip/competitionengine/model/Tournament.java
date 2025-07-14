cb8896ad63d8cb8451381b579617e805a499293c
Roman <raksenov50@gmail.com>
2025-07-14 10:31:47 +0300

    Fixed statistic send.

--- src/main/java/com/drip/competitionengine/model/Tournament.java
+++ src/main/java/com/drip/competitionengine/model/Tournament.java
@@ -1,4 +1,4 @@
-package com.drip.competitionengine.model;
+﻿package com.drip.competitionengine.model;
 
 import com.drip.competitionengine.bracket.*;
 import com.fasterxml.jackson.annotation.JsonIgnore;
